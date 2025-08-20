17f527d4f63f85311d38291e07fb83ea699e46ad
Simple Developer <alina9675@gmail.com>
2025-08-19 23:09:58 -0500

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Use __dirname for path resolution
 
 export default defineConfig({
   plugins: [
+    ghPages(),
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
@@ -16,16 +27,17 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: "replit-demo",
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+  "@": path.resolve(__dirname, "client", "src"),
+  "@shared": path.resolve(__dirname, "shared"),
+  "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
